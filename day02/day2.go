cc2be2be92ff6260f2705916729570dd86f8c969
Dmitry Polshakov <dmitry.polshakov@dsr-corporation.com>
2024-12-02 12:18:54 +0300

    Day2 - part1

--- day02/day2.go
+++ day02/day2.go
@@ -21,52 +21,30 @@ func abs(x int) int {
 	}
 }
 
-func pairIsGood(a int, b int, mustBeAscending bool) bool {
-	ascending := a < b
-	descending := a > b
-	diff := abs(a - b)
-
-	return (1 <= diff && diff <= 3) &&
-		(ascending && mustBeAscending || descending && !mustBeAscending)
-}
-
-func countErrors(report []int, mustBeAscending bool) int {
-	count := 0
-	for i := range len(report) - 1 {
-		if pairIsGood(report[i], report[i+1], mustBeAscending) {
-			// Great case
-			continue
-		}
-
-		// One error is allowed, let's check if the same condition work if one element is skipped
-
-		// If this is the last but one element, we can drop the next element (last) without any check
-		if i == len(report)-2 {
-			count += 1
-			continue
-		}
-
-		if pairIsGood(report[i], report[i+2], mustBeAscending) {
-			count += 1
-			continue
-		}
-
-		// If it doesn't work even with the skipped element - report is bad.
-		// Say that we have a lot of errors
-		return 99
-	}
-
-	// If we faced only one bad element, it's ok
-	return count
-}
-
-func CheckReport(report []int) bool {
+func reportIsSafe(report []int) bool {
 	if len(report) < 2 {
 		return true
 	}
 
-	return countErrors(report, true) <= 1 || countErrors(report, false) <= 1 ||
-		countErrors(report[1:], true) == 0 || countErrors(report[1:], false) == 0
+	// The levels are either all increasing or all decreasing
+	if report[0] == report[1] {
+		return false
+	}
+
+	mustBeAscending := report[0] < report[1]
+
+	for i := range len(report) - 1 {
+		ascending := report[i] < report[i+1]
+		descending := report[i] > report[i+1]
+		diff := abs(report[i] - report[i+1])
+		if (1 <= diff && diff <= 3) && (ascending && mustBeAscending || descending && !mustBeAscending) {
+			continue
+		}
+
+		return false
+	}
+
+	return true
 }
 
 func main() {
@@ -94,13 +72,10 @@ func main() {
 
 	count := 0
 	for _, report := range reports {
-		fmt.Printf("%v -> %v\n", report, CheckReport(report))
-		if CheckReport(report) {
+		if reportIsSafe(report) {
 			count += 1
 		}
 	}
 
 	fmt.Printf("Count: %v", count)
 }
-
-// 424 - 500
